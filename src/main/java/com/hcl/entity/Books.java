4ac2bc82f5dc4d49b3c544bf7ee7f245b905dbc5
naveen19dasari <59041746+naveen19dasari@users.noreply.github.com>
2019-12-25 11:41:52 +0530

    Add files via upload

---
+++ src/main/java/com/hcl/entity/Books.java
@@ -0,0 +1,65 @@
+package com.hcl.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Books {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int bookId;
+	private String bookName;
+	private String author;
+	private double bookPrice;
+	private int bookQuantity;
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(double bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	public int getBookQuantity() {
+		return bookQuantity;
+	}
+
+	public void setBookQuantity(int bookQuantity) {
+		this.bookQuantity = bookQuantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Books [bookId=" + bookId + ", bookName=" + bookName + ", author=" + author + ", bookPrice=" + bookPrice
+				+ ", bookQuantity=" + bookQuantity + "]";
+	}
+
+}
